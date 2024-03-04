e0a46bb60c3b1818710995112cb43b4c73d98d16
Mohammod Arafat Mollik <arafathossain923@gmail.com>
2024-03-04 11:05:16 +0200

    Change the api

--- components/Dashboard/E-commerce.tsx
+++ components/Dashboard/E-commerce.tsx
@@ -12,6 +12,7 @@ import {FaBus} from "react-icons/fa6";
 import {LiaTruckSolid} from "react-icons/lia";
 import {IoMdPeople} from "react-icons/io";
 import flatpickr from "flatpickr";
+import {initialEnv} from "@next/env";
 
 
 const ECommerce: React.FC = () => {
@@ -61,7 +62,7 @@ const ECommerce: React.FC = () => {
         const endDate = endDateRef.current.value;
 
         // First API call to getTrafficDataByDate
-        fetch('/api/getTrafficDataByDate/', {
+        fetch('/api/getTotalTrafficDataFromStat/', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -99,74 +100,76 @@ const ECommerce: React.FC = () => {
     return (
         <>
             {/* <!-- Time and date --> */}
-                <div
-                    className="mb-4 rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
-                    <div className="border-b border-stroke py-4 px-6.5 dark:border-strokedark">
-                        <h3 className="font-medium text-black dark:text-white">
-                            Filter by time and date
-                        </h3>
+            <div
+                className="mb-4 rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+                <div className="border-b border-stroke py-4 px-6.5 dark:border-strokedark">
+                    <h3 className="font-medium text-black dark:text-white">
+                        Filter by time and date
+                    </h3>
+                </div>
+                <div className="flex flex-col gap-5.5 p-6.5">
+
+                    <div>
+                        <label className="mb-3 block text-black dark:text-white">
+                            Set start and end date
+                        </label>
+                        <div className="relative">
+                            <input ref={startDateRef} placeholder="Start Date"
+                                   className="custom-input-date custom-input-date-1 my-4 w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"/>
+                            <input ref={endDateRef} placeholder="End Date"
+                                   className="custom-input-date custom-input-date-1 my-4 w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"/>
+                        </div>
                     </div>
-                    <div className="flex flex-col gap-5.5 p-6.5">
 
-                        <div>
-                            <label className="mb-3 block text-black dark:text-white">
-                                Set start and end date
-                            </label>
-                            <div className="relative">
-                                <input ref={startDateRef} placeholder="Start Date"
-                                       className="custom-input-date custom-input-date-1 my-4 w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"/>
-                                <input ref={endDateRef} placeholder="End Date"
-                                       className="custom-input-date custom-input-date-1 my-4 w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"/>
-                            </div>
-                        </div>
+                    <div className="flex justify-start gap-3">
 
-                        <div className="flex justify-start gap-3">
-
-                            <button
-                                className="max-w-xs rounded bg-primary py-2 px-4 text-white hover:bg-primary-dark"
-                                onClick={handleSubmit}
-                            >
-                                Submit
-                            </button>
-                        </div>
-
+                        <button
+                            className="max-w-xs rounded bg-primary py-2 px-4 text-white hover:bg-primary-dark"
+                            onClick={handleSubmit}
+                        >
+                            Submit
+                        </button>
                     </div>
 
                 </div>
+
+            </div>
             {/* <!-- Chat card --> */}
 
-                <div className="grid grid-cols-2 gap-4 md:grid-cols-3 md:gap-6 xl:grid-cols-3 2xl:gap-7.5">
-                    <CardDataStats title="Total People" total={trafficData.totalPersons.toString()} rate=''>
-                        <IoMdPeople size={'2em'} color="#845EC2"/>
-                    </CardDataStats>
-                    <CardDataStats title="Total Bicycles" total={trafficData.totalBicycles.toString()} rate=''>
-                        <MdDirectionsBike size={'2em'} color="#D65DB1"/>
-                    </CardDataStats>
-                    <CardDataStats title="Total Cars" total={trafficData.totalCars.toString()} rate=''>
-                        <FaCar size={'2em'} color="#FF6F91"/>
-                    </CardDataStats>
-                    <CardDataStats title="Total Motorbikes" total={trafficData.totalMotorBikes.toString()} rate=''>
-                        <RiMotorbikeFill size={'2em'} color="#FF9671"/>
-                    </CardDataStats>
-                    <CardDataStats title="Total Buses" total={trafficData.totalBus.toString()} rate=''>
-                        <FaBus size={'2em'} color="#FFC75F"/>
-                    </CardDataStats>
-                    <CardDataStats title="Total Trucks" total={trafficData.totalTrucks.toString()} rate=''>
-                        <LiaTruckSolid size={'2em'} color="#FF8066"/>
-                    </CardDataStats>
-                </div>
+            <div className="grid grid-cols-2 gap-4 md:grid-cols-3 md:gap-6 xl:grid-cols-3 2xl:gap-7.5">
+                <CardDataStats title="Total People" total={trafficData.totalPersons.toString()} rate=''>
+                    <IoMdPeople size={'2em'} color="#845EC2"/>
+                </CardDataStats>
+                <CardDataStats title="Total Bicycles" total={trafficData.totalBicycles.toString()} rate=''>
+                    <MdDirectionsBike size={'2em'} color="#D65DB1"/>
+                </CardDataStats>
+                <CardDataStats title="Total Cars" total={trafficData.totalCars.toString()} rate=''>
+                    <FaCar size={'2em'} color="#FF6F91"/>
+                </CardDataStats>
+                <CardDataStats title="Total Motorbikes" total={trafficData.totalMotorBikes.toString()} rate=''>
+                    <RiMotorbikeFill size={'2em'} color="#FF9671"/>
+                </CardDataStats>
+                <CardDataStats title="Total Buses" total={trafficData.totalBus.toString()} rate=''>
+                    <FaBus size={'2em'} color="#FFC75F"/>
+                </CardDataStats>
+                <CardDataStats title="Total Trucks" total={trafficData.totalTrucks.toString()} rate=''>
+                    <LiaTruckSolid size={'2em'} color="#FF8066"/>
+                </CardDataStats>
+            </div>
 
-                <div className="mt-4 w-full">
-                    <ChartOne trafficDataStream={trafficDataStream}/>
-                    <br/>
-                    <ChartThree
-                        persons={trafficData.totalPersons}
-                        bicycles={trafficData.totalBicycles}
-                        cars={trafficData.totalCars}
-                        motorbikes={trafficData.totalMotorBikes}
-                        buses={trafficData.totalBus}
-                        trucks={trafficData.totalTrucks}/>
-                </div>
+            <div className="mt-4 w-full">
+                {process.env.NEXT_PUBLIC_SHOW_LINE_CHART === 'true' &&
+                    <ChartOne trafficDataStream={trafficDataStream}/>}
+
+                <br/>
+                <ChartThree
+                    persons={trafficData.totalPersons}
+                    bicycles={trafficData.totalBicycles}
+                    cars={trafficData.totalCars}
+                    motorbikes={trafficData.totalMotorBikes}
+                    buses={trafficData.totalBus}
+                    trucks={trafficData.totalTrucks}/>
+            </div>
         </>
     );
 };
--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -12,6 +12,15 @@ const Header = (props: {
     return (
         <header className="sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
             <div className="flex flex-grow items-center justify-between px-4 py-4 shadow-2 md:px-6 2xl:px-11">
+                <Link className="flex items-center gap-2 flex-shrink-0 " href="/">
+                    <Image
+                        width={32}
+                        height={32}
+                        src={"/images/logo/logo-icon.svg"}
+                        alt="Logo"
+                    />
+                    <span className="font-bold"> TraffiTrack </span>
+                </Link>
                 <div className="flex items-center gap-2 sm:gap-4 lg:hidden">
                     {/* <!-- Hamburger Toggle BTN --> */}
                     <button
@@ -84,7 +93,9 @@ const Header = (props: {
                     </ul>
 
                     {/* <!-- User Area --> */}
+                    {/*
                     <DropdownUser/>
+                    */}
                     {/* <!-- User Area --> */}
                 </div>
             </div>
